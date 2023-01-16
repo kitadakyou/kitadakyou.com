615cc4417d2cbc0e3926becfb0185b15cfa146d8
Hikaru ETO <kitadakyou@yahoo.co.jp>
2023-01-17 05:16:40 +0900

    レイアウトを変更

---
+++ pages/blog/review-of-as-internet.tsx
@@ -0,0 +1,10 @@
+import Layout from '../../components/layout'
+import ArticleOfMarkdown from '../../components/articleOfMarkdown'
+
+export default function Test1 () {
+  return (
+    <Layout>
+      <ArticleOfMarkdown contentPath="/blog/review-of-as-internet.md" />
+    </Layout>
+  )
+}
