4a91bbfc0fd2a2cc24ff9e7fb9796ebee396b5f5
Hikaru ETO <kitadakyou@yahoo.co.jp>
2023-01-17 05:09:56 +0900

    add firebase config

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,30 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import MyHeader from '../components/myheader'
+import Script from 'next/script'
+import React from 'react'
+import { measurementId } from '../libs/firebase'
+
+const GoogleAnalytics = () => {
+  if (process.env.NODE_ENV === 'production') {
+    return (
+        <React.Fragment>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+        
+              gtag('config', '${measurementId}');
+            `}
+          </Script>
+      </React.Fragment>
+    )
+  }
+}
 
 export default function Document () {
   return (
@@ -10,6 +35,7 @@ export default function Document () {
           <MyHeader />
           <Main />
         </div>
+        { GoogleAnalytics() }
         <NextScript />
       </body>
     </Html>
