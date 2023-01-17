2b764b962daefaa2a0b75341b6b5d249368019ec
Hikaru ETO <kitadakyou@yahoo.co.jp>
2023-01-18 07:06:29 +0900

    諸々修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 // import GenreMenu from '../components/genreMenu'
 import styles from '../styles/components/index.module.css'
 import ContentsTable from '../components/contentsTable'
+import Head from 'next/head'
 
 export default function Index () {
   return (
     <main className={styles.articles}>
+      <Head>
+        <title>KITADAKYOU.COM</title>
+        <meta name="description" content="北田共による個人サイト。記事や制作物に関する発表など。" />
+      </Head>
       {// <GenreMenu />
       }
       <ContentsTable />
