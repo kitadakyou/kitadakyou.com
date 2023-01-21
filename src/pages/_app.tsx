20f6dbf8bdab792c00015a851d787287293a3f70
Hikaru ETO <kitadakyou@yahoo.co.jp>
2023-01-21 21:54:31 +0900

    mdx を使った形に変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import MyHeader from '../components/myheader'
+import MyFooter from 'components/myFooter'
 import '../libs/firebase'
 
 export default function App ({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <MyHeader />
+      <Component {...pageProps} />
+      <MyFooter />
+    </>
+  )
 }
