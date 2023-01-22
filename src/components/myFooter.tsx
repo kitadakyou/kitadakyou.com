05eab25dd2bde930a59b68fa602f0b51f31f3107
Hikaru ETO <kitadakyou@yahoo.co.jp>
2023-01-23 00:17:30 +0900

    footer に GitHubリポジトリへのリンクを追加

--- src/components/myFooter.tsx
+++ src/components/myFooter.tsx
@@ -1,9 +1,17 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/components/myFooter.module.css'
 
 export default function MyFooter () {
   return (
     <footer className={styles.footer}>
-      Copyright © 2023 Kitadakyou All rights reserved.
+      <p>
+        Copyright © 2023 Kitadakyou All rights reserved.
+      </p>
+      <Link href='https://github.com/kitadakyou/kitadakyou.com'>
+        <Image src='/img/github-mark-white.svg' alt='twitter icon' height={30} width={30} />
+      </Link>
+
     </footer>
   )
 }
