a6a80cd7ac4452a29829eaccbdf1390537b65910
Hikaru ETO <kitadakyou@yahoo.co.jp>
2024-09-30 19:31:55 +0900

    試験的機能を無効化

--- next.config.js
+++ next.config.js
@@ -7,9 +7,6 @@ const withMDX = createMDX({
 
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  experimental: {
-    ppr: 'incremental',
-  },
 }
 
 export default withMDX(nextConfig)
