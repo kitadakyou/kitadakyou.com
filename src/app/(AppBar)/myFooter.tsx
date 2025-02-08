c4a5c24fc267913290b6b643a0e0e69296ae7b8c
Hikaru ETO <kitadakyou@yahoo.co.jp>
2025-02-08 22:05:13 +0900

    記事を追加

--- src/app/(AppBar)/myFooter.tsx
+++ src/app/(AppBar)/myFooter.tsx
@@ -5,7 +5,7 @@ export default function MyFooter () {
   return (
     <footer className={styles.footer}>
       <p>
-        Copyright © 2024 Kitadakyou All rights reserved.
+        Copyright © 2025 Kitadakyou All rights reserved.
       </p>
       <a href='https://github.com/kitadakyou/kitadakyou.com' target="_blank" rel="noopener noreferrer">
         <Image src='/img/github-mark-white.svg' alt='twitter icon' height={30} width={30} />
