bfa0fbdd04c637be75a8887576c38a6fccf77a6c
Hikaru ETO <kitadakyou@yahoo.co.jp>
2025-02-10 01:29:32 +0900

    不要になったコンポーネントを統合

---
+++ src/app/(AppBar)/blog/[path]/OtherContentLink.tsx
@@ -0,0 +1,26 @@
+import { getCurrentIndex, getContentNum, getPathByIndex } from 'app/(AppBar)/(Contents)/contents'
+import Link from 'next/link'
+
+interface Props {
+  slug: string
+}
+
+export default function OtherContentLink({ slug }: Props): React.ReactNode {
+  const currentIndex = getCurrentIndex(slug)
+  const contetsNum = getContentNum()
+  const hasPreviousLink = currentIndex >= 0 && contetsNum - 1 > currentIndex
+  const hasNextLink = currentIndex > 0
+  return (
+    <>
+      <div>
+        { hasPreviousLink && <Link href={getPathByIndex(currentIndex + 1)}>前の記事へ</Link> }
+      </div>
+      <div>
+        <Link href='/'>一覧へ</Link>
+      </div>
+      <div>
+      { hasNextLink && <Link href={getPathByIndex(currentIndex - 1)}>次の記事へ</Link> }
+      </div>
+    </>
+  )
+}
---
+++ src/app/(AppBar)/blog/[path]/SnsShareLinks.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link"
+import Image from "next/image"
+import styles from './styles.module.css'
+
+interface Props {
+  title: string,
+  slug: string
+}
+
+export default function SnsShareLinks({title, slug}: Props): React.ReactNode {
+  return (
+    <Link href={`https://twitter.com/intent/tweet?text=${title}%20https%3A%2F%2Fkitadakyou.com%2Fblog%2F${slug}%20%23北田共のブログ`}>
+      <button className={styles.twitterButton}>
+        <span>
+          <Image src='/img/twitter-icon-white.svg' alt='twitter icon' width={25} height={25} />
+        </span>Twitter(自称 X)でシェアする
+      </button>
+    </Link>
+  )
+}
