9b7adc81a5e68add6f4632c29c06f5ba671d5593
Hikaru ETO <kitadakyou@yahoo.co.jp>
2025-02-09 21:29:14 +0900

    個別にPATHを作らなくていいように tsx ファイルを変更

---
+++ src/app/(AppBar)/blog/[path]/page.tsx
@@ -0,0 +1,49 @@
+import LayoutContent from '../layoutContent'
+import { findContentDataByPath } from 'app/(AppBar)/(Contents)/contents'
+import { Metadata } from 'next'
+
+interface PageProps {
+  params: {
+    path: string
+  }
+}
+
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const path = (await params).path
+  const metadata = findContentDataByPath(path)
+
+  if (!metadata) {
+    return { title: 'Not Found' }
+  }
+
+  return {
+    title: metadata.title,
+    openGraph: {
+      images: [{
+        url: `/api/og/${path}`,
+        width: 1200,
+        height: 630,
+      }],
+    },
+  }
+}
+
+export default async function Page({ params }: PageProps) {
+  const path = (await params).path
+  const metadata = findContentDataByPath(path)
+
+  console.log('metadata', metadata)
+
+  if (!metadata) return null
+
+  const Article = (await import(`./${path}.mdx`)).default
+  console.log('Article', Article)
+  return (
+    <LayoutContent 
+      title={metadata.title}
+      path={path}
+    >
+      <Article />
+    </LayoutContent>
+  )
+}
--- src/app/(AppBar)/blog/layoutContent.tsx
+++ src/app/(AppBar)/blog/layoutContent.tsx
@@ -1,13 +1,12 @@
 import { ReactNode } from 'react'
 import styles from './layoutContent.module.css'
-import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
 import { contents } from '../(Contents)/contents'
 
 export default function LayoutContent ({ children, title, path, image }: { children: ReactNode, title: string, path: string, image?: string }) {
   const getOtherContentLink = () => {
-    const currentIndex = contents.findIndex(elem => elem.path === `/${elem.genre}/${path}`)
+    const currentIndex = contents.findIndex(elem => elem.path === `/blog/${path}`)
     const hasPreviousLink = currentIndex >= 0 && contents.length - 1 > currentIndex
     const hasNextLink = currentIndex > 0
     return (
@@ -25,24 +24,16 @@ export default function LayoutContent ({ children, title, path, image }: { child
     )
   }
   return (
-    <>
-      <Head>
-        <title>{`${title} - KITADAKYOU.COM`}</title>
-        <meta property="og:title" content={title} />
-        <meta property="og:url" content={`https://kitadakyou.com/blog/${path}`} />
-        { image ? <meta property="og:image" content={image} /> : ''}
-      </Head>
-      <main className={styles.main}>
-        <article className={styles.article}>
-          {children}
-        </article>
-        <section className={styles.shareSection}>
-          <Link href={`https://twitter.com/intent/tweet?text=${title}%20https%3A%2F%2Fkitadakyou.com%2Fblog%2F${path}%20%23北田共のブログ`}><button className={styles.twitterButton}><span><Image src='/img/twitter-icon-white.svg' alt='twitter icon' width={25} height={25} /></span>Twitterでシェアする</button></Link>
-        </section>
-        <section className={styles.linkSection}>
-          { getOtherContentLink() }
-        </section>
-      </main>
-    </>
+    <main className={styles.main}>
+      <article className={styles.article}>
+        {children}
+      </article>
+      <section className={styles.shareSection}>
+        <Link href={`https://twitter.com/intent/tweet?text=${title}%20https%3A%2F%2Fkitadakyou.com%2Fblog%2F${path}%20%23北田共のブログ`}><button className={styles.twitterButton}><span><Image src='/img/twitter-icon-white.svg' alt='twitter icon' width={25} height={25} /></span>Twitter(自称 X)でシェアする</button></Link>
+      </section>
+      <section className={styles.linkSection}>
+        { getOtherContentLink() }
+      </section>
+    </main>
   )
 }
