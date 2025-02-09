44606bdf8b81e133917845f87ea67e49a2991f33
Hikaru ETO <kitadakyou@yahoo.co.jp>
2025-02-10 01:33:40 +0900

    外部サイトは別タブで開くよう修正

--- src/app/(AppBar)/blog/[path]/SnsShareLinks.tsx
+++ src/app/(AppBar)/blog/[path]/SnsShareLinks.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export default function SnsShareLinks({title, slug}: Props): React.ReactNode {
   return (
-    <Link href={`https://twitter.com/intent/tweet?text=${title}%20https%3A%2F%2Fkitadakyou.com%2Fblog%2F${slug}%20%23北田共のブログ`}>
+    <Link href={`https://twitter.com/intent/tweet?text=${title}%20https%3A%2F%2Fkitadakyou.com%2Fblog%2F${slug}%20%23北田共のブログ`} target='_blank'>
       <button className={styles.twitterButton}>
         <span>
           <Image src='/img/twitter-icon-white.svg' alt='twitter icon' width={25} height={25} />
